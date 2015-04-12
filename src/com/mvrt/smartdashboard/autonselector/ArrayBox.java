a3a580f2e10bb06320da8bd0f1a1ae480f053134
Lee Mracek <lee.mracek@gmail.com>
2015-04-11 23:43:53 -0700

    Added presets

--- src/com/mvrt/smartdashboard/autonselector/ArrayBox.java
+++ src/com/mvrt/smartdashboard/autonselector/ArrayBox.java
@@ -1,4 +1,5 @@
 package com.mvrt.smartdashboard.autonselector;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -40,27 +41,27 @@ public class ArrayBox extends StaticWidget {
 	private JButton add;
 	private JButton remove;
 	private JButton save;
-	
+
 	public static final String NAME = "Auton Scripting";
-	
+
 	@Override
 	public void init() {
 		add = new JButton("Add");
 		add.addActionListener(new ActionListener() {
-		
+
 			public void actionPerformed(ActionEvent e) {
 				NewAutonEntryDialog dialog = new NewAutonEntryDialog();
 				dialog.show(remove.getLocationOnScreen());
-				if(!dialog.isCanceled()) {
+				if (!dialog.isCanceled()) {
 					model.add(dialog.getScript());
 					model.fireTableDataChanged();
 				}
 			}
 		});
-		
+
 		remove = new JButton("Remove");
 		remove.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
 				if (table.isEditing()) {
 					table.getCellEditor().cancelCellEditing();
@@ -69,64 +70,66 @@ public class ArrayBox extends StaticWidget {
 				model.fireTableDataChanged();
 			}
 		});
-		
+
 		save = new JButton("Save");
 		save.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				Robot.getTable("AUTONOMOUS").putValue("Auton Array", values);
+				Robot.getTable("AUTONOMOUS").putString("Auton Array",
+						values.toString());
 			}
 		});
-		
+
 		values = new ArrayList<String>();
-		
+
 		model = new AutonTableModel();
-		
+
 		table = new JTable(model);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.getTableHeader().setReorderingAllowed(true);
-		
+
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(0, 2));
 		buttonPanel.add(add);
 		buttonPanel.add(remove);
-		
+
 		JPanel controlPanel = new JPanel();
 		controlPanel.setLayout(new BorderLayout());
 		controlPanel.add(buttonPanel, BorderLayout.NORTH);
-		
+
 		JButton load = new JButton("Load");
 		load.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				InputStream is = this.getClass().getClassLoader().getResourceAsStream("presets.txt");
+				InputStream is = this.getClass().getClassLoader()
+						.getResourceAsStream("presets.txt");
 				NewPresetLoadDialog dialog = new NewPresetLoadDialog(is);
 				dialog.show(remove.getLocationOnScreen());
-				if(!dialog.isCanceled()) {
-					for(String line : dialog.getPreset())
+				if (!dialog.isCanceled()) {
+					for (String line : dialog.getPreset())
 						model.add(line);
 					model.fireTableDataChanged();
 				}
 			}
-			
+
 		});
 		JPanel savePanel = new JPanel();
 		savePanel.setLayout(new GridLayout(2, 0));
 		savePanel.add(load);
 		savePanel.add(save);
 		controlPanel.add(savePanel, BorderLayout.SOUTH);
-		
+
 		setLayout(new BorderLayout());
 		JScrollPane tableScroll = new JScrollPane(table,
 				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		add(tableScroll, BorderLayout.CENTER);
 		add(controlPanel, BorderLayout.SOUTH);
-		
+
 		setPreferredSize(new Dimension(300, 200));
 	}
-	
+
 	private class AutonTableModel extends AbstractTableModel {
 
 		@Override
@@ -136,6 +139,7 @@ public class ArrayBox extends StaticWidget {
 			}
 			return "ERROR";
 		}
+
 		@Override
 		public int getRowCount() {
 			return values.size();
@@ -150,42 +154,43 @@ public class ArrayBox extends StaticWidget {
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			return values.get(rowIndex);
 		}
-		
+
 		@Override
 		public void setValueAt(Object value, int rowIndex, int columnIndex) {
 			values.set(rowIndex, value.toString());
 		}
-		
+
 		public void delete(int index) {
 			values.remove(index);
 		}
-		
+
 		public void add(String script) {
 			values.add(script);
 		}
 	}
-	
+
 	private class NewAutonEntryDialog extends JDialog {
 		private JTextField script;
 		private JButton addButton;
 		private JButton cancelButton;
-		
+
 		boolean canceled = true;
-		
+
 		public NewAutonEntryDialog() {
 			setTitle("New Autonomous Entry");
 			setModal(true);
 			setResizable(false);
-			
-			((JComponent) getContentPane()).setBorder(BorderFactory.createEmptyBorder(7, 7, 7, 7));
-			
+
+			((JComponent) getContentPane()).setBorder(BorderFactory
+					.createEmptyBorder(7, 7, 7, 7));
+
 			setLayout(new GridBagLayout());
 			GridBagConstraints c = new GridBagConstraints();
-			
+
 			add(new JLabel("Command: "), c);
 			c.gridx = 1;
 			add(script = new JTextField(20), c);
-			
+
 			JPanel buttonPanel = new JPanel();
 			buttonPanel.setLayout(new GridLayout(0, 2));
 			buttonPanel.add(addButton = new JButton("Add"), c);
@@ -197,7 +202,7 @@ public class ArrayBox extends StaticWidget {
 				}
 			});
 			getRootPane().setDefaultButton(addButton);
-			
+
 			buttonPanel.add(cancelButton = new JButton("Cancel"));
 			cancelButton.addActionListener(new ActionListener() {
 
@@ -210,74 +215,78 @@ public class ArrayBox extends StaticWidget {
 			c.gridy = 1;
 			c.gridwidth = 2;
 			add(buttonPanel, c);
-			
+
 			pack();
 		}
-		
+
 		public void show(Point center) {
-			setLocation((int) (center.getX() - getWidth() / 2), (int) (center.getY() - getHeight() / 2));
+			setLocation((int) (center.getX() - getWidth() / 2),
+					(int) (center.getY() - getHeight() / 2));
 			setVisible(true);
 		}
-		
+
 		public boolean isCanceled() {
 			return canceled;
 		}
-		
+
 		public String getScript() {
 			return script.getText();
 		}
 	}
-	
+
 	private class NewPresetLoadDialog extends JDialog {
 		JList<String> list;
 		private JButton addButton;
 		private JButton cancelButton;
 		private boolean canceled = true;
 		Map<String, ArrayList<String>> presets;
-		
+
 		public NewPresetLoadDialog(InputStream is) {
 			setTitle("New Autonomous Entry");
 			setModal(true);
 			setResizable(false);
-			
-			((JComponent) getContentPane()).setBorder(BorderFactory.createEmptyBorder(7, 7, 7, 7));
-			
+
+			((JComponent) getContentPane()).setBorder(BorderFactory
+					.createEmptyBorder(7, 7, 7, 7));
+
 			InputStreamReader ir = new InputStreamReader(is);
 			BufferedReader read = new BufferedReader(ir);
 			String line;
 			presets = new HashMap<String, ArrayList<String>>();
 			try {
-			while((line = read.readLine()) != null) {
-				String name = line;
 				line = read.readLine();
-				ArrayList<String> temp = new ArrayList<String>();
-				while(line != null && line.charAt(0) == '\t') {
-					temp.add(line);
+				while (line != null) {
+					String name = line;
 					line = read.readLine();
-			}
-				presets.put(name, temp);
-			}
+					ArrayList<String> temp = new ArrayList<String>();
+					while (line != null && line.charAt(0) == '\t') {
+						temp.add(line);
+						line = read.readLine();
+					}
+					presets.put(name, temp);
+				}
 			} catch (IOException io) {
 				ArrayList<String> temp = new ArrayList<String>();
 				temp.add("Test");
 				presets.put("ERROR", temp);
 			}
-			String[] listarr = presets.keySet().toArray(new String[presets.size()]);
-			
+			String[] listarr = presets.keySet().toArray(
+					new String[presets.size()]);
+
 			setLayout(new GridBagLayout());
 			GridBagConstraints c = new GridBagConstraints();
-			
+
 			list = new JList<String>(listarr);
 			list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
 			list.setVisibleRowCount(-1);
-			
+
 			JScrollPane listScroll = new JScrollPane(list);
 			listScroll.setPreferredSize(new Dimension(250, 80));
-			
+
 			c.gridx = 1;
 			add(listScroll);
-			
+
 			JPanel buttonPanel = new JPanel();
 			buttonPanel.setLayout(new GridLayout(0, 2));
 			buttonPanel.add(addButton = new JButton("Add"), c);
@@ -289,7 +298,7 @@ public class ArrayBox extends StaticWidget {
 				}
 			});
 			getRootPane().setDefaultButton(addButton);
-			
+
 			buttonPanel.add(cancelButton = new JButton("Cancel"));
 			cancelButton.addActionListener(new ActionListener() {
 
@@ -302,19 +311,20 @@ public class ArrayBox extends StaticWidget {
 			c.gridy = 1;
 			c.gridwidth = 2;
 			add(buttonPanel, c);
-			
+
 			pack();
 		}
-		
+
 		public void show(Point center) {
-			setLocation((int) (center.getX() - getWidth() / 2), (int) (center.getY() - getHeight() / 2));
+			setLocation((int) (center.getX() - getWidth() / 2),
+					(int) (center.getY() - getHeight() / 2));
 			setVisible(true);
 		}
-		
+
 		public boolean isCanceled() {
 			return canceled;
 		}
-		
+
 		public String[] getPreset() {
 			ArrayList<String> arrList = presets.get(list.getSelectedValue());
 			return arrList.toArray(new String[arrList.size()]);
